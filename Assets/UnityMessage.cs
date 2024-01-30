e09fc68e11a23466dca839c5d2798290d9b09c08
bongsu <rlaqhdtn22@gmail.com>
2024-01-30 15:07:16 +0900

    docs : 게임오브젝트

---
+++ Assets/UnityMessage.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UnityMessage : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start() // 준비
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update() // 동작
+    {
+        
+    }
+
+    void OnCollisionEnter(Collision collision)
+    {
+        
+    }
+}
